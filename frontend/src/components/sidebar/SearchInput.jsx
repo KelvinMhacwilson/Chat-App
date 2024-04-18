a466bdcabe30a5ba3913e287de61c3c4c7bab570
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-04-18 21:03:15 +0000

    Added Search

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (search === "") return;
+    if (search.length < 3) {
+      return toast.error("Search term must be at least 3 characters long");
+    }
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+    } else toast.error("No such user found!");
+  };
   return (
-    <form className="flex items-center gap-2">
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
       <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         type="text"
         placeholder="Search.."
         className="input input-bordered rounded-full"
